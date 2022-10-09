f6d75afec46e1905ec4f6202e603ba350ea8d614
Rafael <rafael.leviis@gmail.com>
2022-10-09 13:42:16 -0300

    Inclusão do Requisito do Exercício 1

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 public class App {
+    //Requisito 1: Faça um Programa que mostre a mensagem "Alo mundo" na tela
     public static void main(String[] args) throws Exception {
         System.out.println("Alo Mundo");
     }
